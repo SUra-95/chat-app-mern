f73722021b62a4b2caf1861ee03d13b17f0a553a
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-11-28 23:48:36 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -10,4 +10,9 @@ app.get('/', (req, res) => {
     res.send('Hello, world!!!!!!!!!!!!!!!!!!!!!!!!');
 });
 
+app.get('/api/auth/signup', (req, res) => {
+    console.log('signup route');
+    
+});
+
 app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)});
