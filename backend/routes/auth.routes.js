40f67e8a158eca6cd708ea5db19eb2702d3d0ae3
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-11-29 00:19:30 +0530

    .

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
+router.get('/signup', (req, res) => {
+    res.send("signup route");
+});
 router.get('/login', (req, res) => {
     res.send("login route");
 });
