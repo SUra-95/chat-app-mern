f9ae96e145c1311f15b2a4d3b3bc4b5d9ae1fd83
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-12-06 23:54:48 +0530

    .

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,1 +1,9 @@
-export const ign
+export const login = (req, res, next) => {
+    console.log("Login User");
+}
+export const logout = (req, res, next) => {
+    console.log("Logout User");
+}
+export const signup = (req, res, next) => {
+    console.log("Sign up");
+}
