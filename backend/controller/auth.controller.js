7c3e393cf0eb3853b57561f9500f276ce3d39bff
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-12-07 20:54:06 +0530

    connected to mongodb connection

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,9 +1,14 @@
+export const signup = async (req, res, next) => {
+    try {
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+
+    } catch (error) {
+        
+    }    
+}
 export const login = (req, res, next) => {
     console.log("Login User");
 }
 export const logout = (req, res, next) => {
     console.log("Logout User");
-}
-export const signup = (req, res, next) => {
-    console.log("Sign up");
 }
