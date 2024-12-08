494a8180396d33c12dbb149f16c3a8d32163a3af
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-12-09 01:08:26 +0530

    routes protected with a middleware

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,7 +1,10 @@
 export const sendMessage = (req, res) => {
     try {
         const {message} = req.body; // const message = req.body.message;
-        const {id} = req.params; //const id = req.params.id;
+        const {id: receiverId} = req.params; //const id = req.params.id;
+        const senderId = req.user._id;
+
+
     } catch (error) {
         console.log("Error in message Controller", error.message);
         res.status(500).json({ error: "Internal Server Error"});
--- backend/middleware/protectRoutes.js
+++ backend/middleware/protectRoutes.js
@@ -1,10 +1,24 @@
 
-const protectRoutes = (req, res, next) => {
-    const token = req.cookies.jwt;
+const protectRoutes = async(req, res, next) => {
 
+    const token = req.cookies.jwt;
     if(!token) {
        return res.status(401).json({error: "Unauthorized - No token provided"});
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if(!decoded){
+        return res.status(401).json({error: "Unauthorized - invalid token provided"});
+    }
+
+    const user = await User.findById(decode.userId).select("-password"); 
+    if(!user){
+        return res.status(401).json({error: "User not found"});
+    }
+
+    req.user = user;
+
+    next();
 };
 
 export default protectRoutes;
