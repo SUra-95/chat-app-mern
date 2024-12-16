d1057ac464ada304bc27f651f4322cefc3ce4430
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-12-16 23:58:02 +0530

    .

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,9 +1,14 @@
-export const sendMessage = (req, res) => {
+import Conversation from "../models/conversation.model.js";
+
+export const sendMessage = async (req, res) => {
     try {
         const {message} = req.body; // const message = req.body.message;
         const {id: receiverId} = req.params; //const id = req.params.id;
         const senderId = req.user._id;
 
+            await Conversation.findOne({
+                participants: { all }
+            });
 
     } catch (error) {
         console.log("Error in message Controller", error.message);
