15e5bdf65bbbaa679162e5d73991d715ca5ca539
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-12-18 00:00:27 +0530

    .

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -22,6 +22,10 @@ export const sendMessage = async (req, res) => {
             message
         })
 
+        if (newMessage){
+            conversation.messages.push(newMessage._id);
+        }
+
     } catch (error) {
         console.log("Error in message Controller", error.message);
         res.status(500).json({ error: "Internal Server Error"});
