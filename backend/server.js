b9edc9979f09e7e827afcdf49f33e26e3b71685a
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-12-19 22:59:21 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
-import protectRoutes from '../backend/middleware/protectRoutes.js'
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -17,6 +18,7 @@ app.use(cookieParser()); // to parse the incoming cookies from req.cookie
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 // app.get('/', (req, res) => {
 //     res.send('Hello, world!!!!!!!!!!!!!!!!!!!!!!!!');
