370188f65687613ee989591e8b0e77bb01511565
Suranjan De Silva <suranjandesilva1995@gmail.com>
2025-01-10 14:42:29 +0530

    socket.io

---
+++ backend/socket/socket.js
@@ -0,0 +1,25 @@
+import { Server } from 'socket.io';
+import http from 'http';
+import express from 'express';
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected', socket.id);
+
+    // socket.on() is used to listen for the events. can be used bith on client and server side
+    socket.on('disconnect', (socket) => {
+        console.log('A user disconnected', socket.id);
+    });
+});
+
+
+export { app, io, server };
