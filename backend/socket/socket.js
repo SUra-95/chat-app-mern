4ea8367a98090fdf16565fb6606be061b2638a0d
Suranjan De Silva <suranjandesilva1995@gmail.com>
2025-01-11 23:59:54 +0530

    .

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -20,6 +20,9 @@ io.on('connection', (socket) => {
     const userId = socket.handshake.query.userId;
     if (userId != "undefined") userSocketMap[userId] = socket.id;
 
+    // io.emit() is used to send events to all the connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
     // socket.on() is used to listen for the events. can be used bith on client and server side
     socket.on('disconnect', (socket) => {
         console.log('A user disconnected', socket.id);
